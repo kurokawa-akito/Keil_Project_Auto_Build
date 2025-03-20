68305cfd51653c4efaf36860305344c0264f1325
kurokawa-akito <71059119+kurokawa-akito@users.noreply.github.com>
2025-03-20 09:56:07 +0800

    Update autoMPLABv3.py

--- autoMPLABv3.py
+++ autoMPLABv3.py
@@ -1,12 +1,10 @@
 import subprocess
 
-# 編譯器與目標設定
 compiler = r"C:\Program Files\Microchip\xc32\v4.45\bin\xc32-gcc.exe"
 source_file = r"D:\spg_ta010_asymm_auth_genkey\spg_ta010_asymm_auth\firmware\asymm_auth_firmware\src\main.c"
 elf_file = r"D:\spg_ta010_asymm_auth_genkey\spg_ta010_asymm_auth\firmware\output.elf"
 hex_file = r"D:\spg_ta010_asymm_auth_genkey\spg_ta010_asymm_auth\firmware\spg_ta010_asymm_auth_genkey.hex"
 
-# 包含路徑
 include_dirs = [
     r"D:\spg_ta010_asymm_auth_genkey\spg_ta010_asymm_auth\firmware\asymm_auth_firmware\src",
     r"D:\spg_ta010_asymm_auth_genkey\spg_ta010_asymm_auth\firmware\asymm_auth_firmware\src\config\default\definitions.h",
@@ -16,7 +14,6 @@ include_dirs = [
     r"D:\spg_ta010_asymm_auth_genkey\spg_ta010_asymm_auth\firmware\asymm_auth_firmware\src\packs\CMSIS\CMSIS\Core\Include",
 ]
 
-# 生成編譯標誌
 flags = [
     "-g",
     "-x", "c",
@@ -28,10 +25,9 @@ flags = [
     "-fno-common",
     "-Werror",
     "-Wall",
-    *[f'-I"{inc}"' for inc in include_dirs],  # 添加包含路徑
+    *[f'-I"{inc}"' for inc in include_dirs],
 ]
 
-# 執行編譯命令
 command = [
     compiler,
     *flags,
@@ -39,13 +35,11 @@ command = [
     source_file,
 ]
 
-# 記錄到log檔案
 log_file = r"D:\buildLog.txt"
 with open(log_file, "w") as log:
     process = subprocess.run(command, stdout=log, stderr=log, text=True, shell = True)
 
-# 檢查是否成功
 if process.returncode == 0:
-    print("編譯成功！")
+    print("compile success")
 else:
-    print("編譯失敗，請檢查 log 文件中的錯誤訊息。")
+    print("compile failed，請檢查 log 文件中的錯誤訊息。")
